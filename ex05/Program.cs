b31a78df3f468c7e788fb0b66e89637d7aa1363c
WESLEY DA SILVA DE JESUS <wesley.sjesus7@senacsp.edu.br>
2025-01-14 16:58:34 -0300

    exerciocios 04

---
+++ ex05/Program.cs
@@ -0,0 +1,22 @@
+﻿//ex.05 - Solicitar dois numro e apresentar a soma 
+
+Console.WriteLine("Digite o 1 número; ");
+int num1 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Digite o 2 número; ");
+int num2 = int.Parse(Console.ReadLine());
+
+
+Console.Clear();
+
+Console.WriteLine("======================");
+Console.WriteLine("===Soma de números ===");
+Console.WriteLine("======================");
+
+
+Console.WriteLine("$ A soma é: {num1 + num2} ");
+
+
+
+
--- ex05/obj/Debug/net8.0/ex05.AssemblyInfo.cs
+++ ex05/obj/Debug/net8.0/ex05.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ex02")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2a8095c1bc1bcaa5ce93a5cd2e23f3f60c9577")]
-[assembly: System.Reflection.AssemblyProductAttribute("ex02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ex02")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex05")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
